97b443f460f3bbc301e73e76a8a5c99e7d1a0464
Muthurajchella- <muthuraj.chella@gmail.com>
2022-11-25 09:18:20 +0530

    newProject

---
+++ flight-luncher/src/App.js
@@ -0,0 +1,14 @@
+import { BrowserRouter, Link, Routes, Route } from 'react-router-dom';
+import './App.css';
+import Home from './Pages/Home/Home';
+
+function App() {
+  return (
+    <div className="App">
+      <Home />
+      {/* <Header />  */}
+    </div>
+  );
+}
+
+export default App;
